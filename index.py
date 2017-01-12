c136e62392bf827a311b072e32bcba3c915b42fd
zeeshan khan <zeeshank606@gmail.com>
2017-01-12 20:32:54 +0530

    i have made a python project

---
+++ index.py
@@ -0,0 +1,22 @@
+t=int(raw_input())
+for y in range(t):
+   a=[]
+   k=int(raw_input())
+   for i in range(k):
+      m= int(raw_input())
+      a.append(m)
+   x=0
+   for j in range(k):
+       x=(2**x)-a[j]
+       if (x<0):
+         break
+       if (x==0):
+          if (sum(a[j+1:])!=0):
+            x=1
+            break
+          else : 
+            break  
+   if (x==0):
+       print "Yes"
+   else:
+       print "No"             
